a6dac1da66f8e41204a0aa8e3a7bc712e9d1577b
Hiroki Oshima <geet1876@gmail.com>
2020-08-10 22:34:37 +0900

    Add factories

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module AwesomeEvents
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.factory_bot.definition_file_paths = ["test/factories"]
   end
 end
